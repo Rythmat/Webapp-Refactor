f06fd3f3007d413adf0bb767f5d8986ba1cdb264
Rythmat <teotaniwha@gmail.com>
2025-11-03 15:01:21 -0700

    try type removal

--- src/hooks/data/prism/types.ts
+++ src/hooks/data/prism/types.ts
@@ -1,6 +1,6 @@
-import { Api } from '@/contexts/MusicAtlasContext';
+// import { Api } from '@/contexts/MusicAtlasContext';
 
-export type PrismChordName = Parameters<Api['music']['getPrismChordsByName']>[0];
-export type PrismModeSlug = Parameters<Api['music']['getPrismModesByMode']>[0];
-export type PrismModeFamily =
-  Parameters<Api['music']['getPrismModesFamilyByFamily']>[0];
+// export type PrismChordName = Parameters<Api['music']['getPrismChordsByName']>[0];
+// export type PrismModeSlug = Parameters<Api['music']['getPrismModesByMode']>[0];
+// export type PrismModeFamily =
+//   Parameters<Api['music']['getPrismModesFamilyByFamily']>[0];

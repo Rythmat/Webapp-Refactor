05294e242c9805641e4e978462f6261ecffeda4a
Rythmat <teotaniwha@gmail.com>
2026-02-09 17:40:14 -0700

    Revert "keyboard interactive start"
    This reverts commit 0af4e06bbb10238257d771eda9bddf6b581936ad.

--- src/components/Games/PlayAlong.tsx
+++ src/components/Games/PlayAlong.tsx
@@ -355,13 +355,12 @@ export const PlayAlong = ({
   });
 
   useEffect(() => {
-    if (!isPlaying) return;
     const stop = startListening();
     return () => {
       stop?.();
       stopListening();
     };
-  }, [isPlaying, startListening, stopListening]);
+  }, []);
 
  
 
@@ -439,7 +438,6 @@ export const PlayAlong = ({
             performanceMeta={performanceMeta}
             onTickChange={setCurrentTick}
             startMessage={startMessage}
-            showStartSequence
           />
           <PianoKeyboard
           className="mx-auto"

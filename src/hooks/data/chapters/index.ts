3a483d6981ff3a1d8c6ec5bfc79d8138a6d0514f
Rythmat <teotaniwha@gmail.com>
2026-03-24 18:54:43 -0700

    insider access duration

--- src/hooks/data/chapters/index.ts
+++
@@ -1,6 +0,0 @@
-export { useChapter } from './useChapter';
-export { useChapters, type Chapter } from './useChapters';
-export { useReorderPages } from './useReorderPages';
-export { useCreateChapter } from './useCreateChapter';
-export { useUpdateChapter } from './useUpdateChapter';
-export { useDeleteChapter } from './useDeleteChapter';
--- src/hooks/data/index.ts
+++ src/hooks/data/index.ts
@@ -1,16 +1,11 @@
 // Export all hooks from their respective folders
 export * from './auth';
-export * from './chapters';
 export * from './classrooms';
 export * from './collections';
 export * from './notes';
-export * from './pages';
-export * from './phraseMaps';
-export * from './practiceEvents';
 export * from './prism';
 export * from './students';
 export * from './teachers';
-export * from './playAlongs';
 export * from './progress';
 export * from './credits';
 export * from './subscription';
